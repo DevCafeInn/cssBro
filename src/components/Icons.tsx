09bd26c4e7b12b1188d56c74430831e470372cdb
DJ Hemath <djhemath@gmail.com>
2025-02-23 10:57:36 +0530

    feat: new icons

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -32,4 +32,22 @@ function ResetIcon({ width = 10 }) {
     return <svg width={width} className="svg-inline--fa fa-rotate-left" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="rotate-left" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M48.5 224H40c-13.3 0-24-10.7-24-24V72c0-9.7 5.8-18.5 14.8-22.2s19.3-1.7 26.2 5.2L98.6 96.6c87.6-86.5 228.7-86.2 315.8 1c87.5 87.5 87.5 229.3 0 316.8s-229.3 87.5-316.8 0c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0c62.5 62.5 163.8 62.5 226.3 0s62.5-163.8 0-226.3c-62.2-62.2-162.7-62.5-225.3-1L185 183c6.9 6.9 8.9 17.2 5.2 26.2s-12.5 14.8-22.2 14.8H48.5z"></path></svg>
 }
 
-export { CopyIcon, WrongIcon, ArrowIcon, DimensionIcon,  FontIcon, ResetIcon };
+function AngleIcon({width = 10}) {
+    return (
+        <svg width={width} height="12" viewBox="0 0 11 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <line x1="0.5" y1="10.6627" x2="0.5" y2="0.66272" stroke="#CCCCCC" stroke-linejoin="round"></line>
+          <line y1="10.5309" x2="10.011" y2="10.5309" stroke="#CCCCCC" stroke-linejoin="round"></line>
+          <path d="M0.351067 6.17887C2.00086 5.89892 5.17854 6.33932 4.69097 10.3404" stroke="#CCCCCC"></path>
+        </svg>
+    );
+}
+
+function BorderRadiusIcon({width = 10}) {
+    return (
+        <svg width={width} height="12" viewBox="1 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M10.5 9.5V10.5H9.5V9.5H10.5ZM8.5 9.5V10.5H7.5V9.5H8.5ZM6.5 9.5V10.5H5.5V9.5H6.5ZM4.5 9.5V10.5H3.5V9.5H4.5ZM2.5 9.5V10.5H1.5V9.5H2.5ZM10.5 7.5V8.5H9.5V7.5H10.5ZM2.5 7.5V8.5H1.5V7.5H2.5ZM2.5 5.5V6.5H1.5V5.5H2.5ZM8 1.5C9.3435 1.5 10.441 2.562 10.4975 3.8915L10.5 4V6.5H9.5V4C9.5 3.2045 8.8725 2.5485 8.088 2.5025L8 2.5H5.5V1.5H8ZM2.5 3.5V4.5H1.5V3.5H2.5ZM2.5 1.5V2.5H1.5V1.5H2.5ZM4.5 1.5V2.5H3.5V1.5H4.5Z" fill="#CCCCCC"></path>
+        </svg>
+    );
+}
+
+export { CopyIcon, WrongIcon, ArrowIcon, DimensionIcon,  FontIcon, ResetIcon, AngleIcon, BorderRadiusIcon };
